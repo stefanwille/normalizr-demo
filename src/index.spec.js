b755679e564c7c2758dc535363d9ad0fdb7417f9
Stefan Wille <github@stefanwille.com>
2018-04-08 12:38:26 +0200

    Examples for normalize and denormalize

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,28 +1,55 @@
-import { normalize, schema } from "normalizr";
+const { normalize, denormalize, schema } = require("normalizr");
 
 describe("normalizr", () => {
-  describe("normalize()", () => {
-    const article = schema.Entity("articles");
-    const node = schema.Entity("nodes", {
-      articles: article
-    });
+  const article = new schema.Entity("articles");
+  const node = new schema.Entity("nodes", { articles: [article] });
+  const arrayOfNodes = new schema.Array(node);
 
+  describe("normalize()", () => {
     it("extracts the entities", () => {
       const nodesWithArticles = [
         {
-          id: 1,
+          id: "1",
           title: "Chicken",
           articles: [
             {
-              id: 1001,
+              id: "1001",
               subject: "Greatness in Chicken"
             }
           ]
         }
       ];
 
-      const result = normalize(nodesWithArticles, node);
-      expect(result).toEqual([]);
+      const expected = {
+        entities: {
+          articles: { "1001": { id: "1001", subject: "Greatness in Chicken" } },
+          nodes: { "1": { articles: ["1001"], id: "1", title: "Chicken" } }
+        },
+        result: ["1"]
+      };
+
+      const normalizedData = normalize(nodesWithArticles, arrayOfNodes);
+      expect(normalizedData).toEqual(expected);
+    });
+  });
+
+  describe("denormalize()", () => {
+    it("converts entities into an object tree", () => {
+      const entities = {
+        articles: { "1001": { id: "1001", subject: "Greatness in Chicken" } },
+        nodes: { "1": { articles: ["1001"], id: "1", title: "Chicken" } }
+      };
+
+      const nodeInput = ["1"];
+      const expected = [
+        {
+          articles: [{ id: "1001", subject: "Greatness in Chicken" }],
+          id: "1",
+          title: "Chicken"
+        }
+      ];
+      const denormalizedData = denormalize(nodeInput, arrayOfNodes, entities);
+      expect(denormalizedData).toEqual(expected);
     });
   });
 });
