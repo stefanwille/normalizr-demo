832dba18dec03059e4c9b6f33fae95be429c4f18
Stefan Wille <github@stefanwille.com>
2018-05-05 10:55:15 +0200

    Add example for entity dependent on another entity

--- src/index.spec.js
+++ src/index.spec.js
@@ -27,6 +27,40 @@ describe("normalizr", () => {
       });
     });
 
+    describe("with one entity depending on another entity", () => {
+      it("extracts the entities", () => {
+        const node = {
+          id: "1",
+          title: "Chicken",
+          articles: [
+            {
+              id: "1001",
+              subject: "Greatness in Chicken"
+            }
+          ]
+        };
+
+        const expected = {
+          entities: {
+            articles: {
+              "1001": { id: "1001", subject: "Greatness in Chicken" }
+            },
+            nodes: {
+              "1": {
+                id: "1",
+                title: "Chicken",
+                articles: ["1001"]
+              }
+            }
+          },
+          result: "1"
+        };
+
+        const normalizedData = normalize(node, nodeSchema);
+        expect(normalizedData).toEqual(expected);
+      });
+    });
+
     describe("with an array", () => {
       it("extracts the entities", () => {
         const nodesWithArticles = [
@@ -60,35 +94,45 @@ describe("normalizr", () => {
 
   describe("denormalize()", () => {
     describe("with a single entity", () => {
-      it("converts entities into an object tree", () => {
+      it("converts the entities hash into an object tree", () => {
+        const id = "1001";
         const entities = {
           articles: { "1001": { id: "1001", subject: "Greatness in Chicken" } }
         };
 
-        const articleInput = "1001";
-        const denormalizedData = denormalize(
-          articleInput,
-          articleSchema,
-          entities
-        );
+        const denormalizedData = denormalize(id, articleSchema, entities);
         const expected = { id: "1001", subject: "Greatness in Chicken" };
         expect(denormalizedData).toEqual(expected);
       });
     });
 
-    describe("with an array", () => {
-      it("converts entities into an object tree", () => {
+    describe("with one entity depending on another entity", () => {
+      it("converts the entities hash into an object tree", () => {
+        const id = "1";
         const entities = {
           articles: { "1001": { id: "1001", subject: "Greatness in Chicken" } },
           nodes: { "1": { articles: ["1001"], id: "1", title: "Chicken" } }
         };
 
-        const nodeInput = ["1"];
-        const denormalizedData = denormalize(
-          nodeInput,
-          arrayOfNodesSchema,
-          entities
-        );
+        const denormalizedData = denormalize(id, nodeSchema, entities);
+        const expected = {
+          id: "1",
+          title: "Chicken",
+          articles: [{ id: "1001", subject: "Greatness in Chicken" }]
+        };
+        expect(denormalizedData).toEqual(expected);
+      });
+    });
+
+    describe("with an array", () => {
+      it("converts the entities hash into an object tree", () => {
+        const id = ["1"];
+        const entities = {
+          articles: { "1001": { id: "1001", subject: "Greatness in Chicken" } },
+          nodes: { "1": { articles: ["1001"], id: "1", title: "Chicken" } }
+        };
+
+        const denormalizedData = denormalize(id, arrayOfNodesSchema, entities);
         const expected = [
           {
             id: "1",
