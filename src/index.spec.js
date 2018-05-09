259ae920f997cc83f65950fd3517bf8737211c94
Stefan Wille <github@stefanwille.com>
2018-05-09 20:24:57 +0200

    Replace string ids with numbers

--- src/index.spec.js
+++ src/index.spec.js
@@ -9,17 +9,17 @@ describe("normalizr", () => {
     describe("with a single entity", () => {
       it("extracts the entities", () => {
         const article = {
-          id: "1001",
+          id: 1001,
           subject: "Greatness in Chicken"
         };
 
         const expected = {
           entities: {
             articles: {
-              "1001": { id: "1001", subject: "Greatness in Chicken" }
+              1001: { id: 1001, subject: "Greatness in Chicken" }
             }
           },
-          result: "1001"
+          result: 1001
         };
 
         const normalizedData = normalize(article, articleSchema);
@@ -30,11 +30,11 @@ describe("normalizr", () => {
     describe("with one entity depending on another entity", () => {
       it("extracts the entities", () => {
         const node = {
-          id: "1",
+          id: 1,
           title: "Chicken",
           articles: [
             {
-              id: "1001",
+              id: 1001,
               subject: "Greatness in Chicken"
             }
           ]
@@ -43,17 +43,17 @@ describe("normalizr", () => {
         const expected = {
           entities: {
             articles: {
-              "1001": { id: "1001", subject: "Greatness in Chicken" }
+              1001: { id: 1001, subject: "Greatness in Chicken" }
             },
             nodes: {
-              "1": {
-                id: "1",
+              1: {
+                id: 1,
                 title: "Chicken",
-                articles: ["1001"]
+                articles: [1001]
               }
             }
           },
-          result: "1"
+          result: 1
         };
 
         const normalizedData = normalize(node, nodeSchema);
@@ -65,11 +65,11 @@ describe("normalizr", () => {
       it("extracts the entities", () => {
         const nodesWithArticles = [
           {
-            id: "1",
+            id: 1,
             title: "Chicken",
             articles: [
               {
-                id: "1001",
+                id: 1001,
                 subject: "Greatness in Chicken"
               }
             ]
@@ -79,11 +79,11 @@ describe("normalizr", () => {
         const expected = {
           entities: {
             articles: {
-              "1001": { id: "1001", subject: "Greatness in Chicken" }
+              1001: { id: 1001, subject: "Greatness in Chicken" }
             },
-            nodes: { "1": { articles: ["1001"], id: "1", title: "Chicken" } }
+            nodes: { 1: { articles: [1001], id: 1, title: "Chicken" } }
           },
-          result: ["1"]
+          result: [1]
         };
 
         const normalizedData = normalize(nodesWithArticles, arrayOfNodesSchema);
@@ -95,30 +95,30 @@ describe("normalizr", () => {
   describe("denormalize()", () => {
     describe("with a single entity", () => {
       it("converts the entities hash into an object tree", () => {
-        const id = "1001";
+        const id = 1001;
         const entities = {
-          articles: { "1001": { id: "1001", subject: "Greatness in Chicken" } }
+          articles: { 1001: { id: 1001, subject: "Greatness in Chicken" } }
         };
 
         const denormalizedData = denormalize(id, articleSchema, entities);
-        const expected = { id: "1001", subject: "Greatness in Chicken" };
+        const expected = { id: 1001, subject: "Greatness in Chicken" };
         expect(denormalizedData).toEqual(expected);
       });
     });
 
     describe("with one entity depending on another entity", () => {
       it("converts the entities hash into an object tree", () => {
-        const id = "1";
+        const id = 1;
         const entities = {
-          articles: { "1001": { id: "1001", subject: "Greatness in Chicken" } },
-          nodes: { "1": { articles: ["1001"], id: "1", title: "Chicken" } }
+          articles: { 1001: { id: 1001, subject: "Greatness in Chicken" } },
+          nodes: { 1: { articles: [1001], id: 1, title: "Chicken" } }
         };
 
         const denormalizedData = denormalize(id, nodeSchema, entities);
         const expected = {
-          id: "1",
+          id: 1,
           title: "Chicken",
-          articles: [{ id: "1001", subject: "Greatness in Chicken" }]
+          articles: [{ id: 1001, subject: "Greatness in Chicken" }]
         };
         expect(denormalizedData).toEqual(expected);
       });
@@ -126,18 +126,18 @@ describe("normalizr", () => {
 
     describe("with an array", () => {
       it("converts the entities hash into an object tree", () => {
-        const id = ["1"];
+        const id = [1];
         const entities = {
-          articles: { "1001": { id: "1001", subject: "Greatness in Chicken" } },
-          nodes: { "1": { articles: ["1001"], id: "1", title: "Chicken" } }
+          articles: { 1001: { id: 1001, subject: "Greatness in Chicken" } },
+          nodes: { 1: { articles: [1001], id: 1, title: "Chicken" } }
         };
 
         const denormalizedData = denormalize(id, arrayOfNodesSchema, entities);
         const expected = [
           {
-            id: "1",
+            id: 1,
             title: "Chicken",
-            articles: [{ id: "1001", subject: "Greatness in Chicken" }]
+            articles: [{ id: 1001, subject: "Greatness in Chicken" }]
           }
         ];
         expect(denormalizedData).toEqual(expected);
